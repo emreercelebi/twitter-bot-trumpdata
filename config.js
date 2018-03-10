dad8bc837e714ffe33ade561dfeb3dadf019012e
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2018-03-10 00:02:41 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+
+Placeholder file. Actual config.js file was not pushed to protect API keys.
+Actual config file follows format:
+
+var config = {
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+};
+
+*/
